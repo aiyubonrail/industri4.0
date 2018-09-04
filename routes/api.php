a382d856efc306d0c6e9e3994d65ba53abe5bbc2
Aiyub <aiyubmhd@gmail.com>
2018-09-04 15:43:11 +0700

    Industrial Revolution 4.0 Perusahaan

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/perusahaan','CompanyController');
+Route::group(['prefix'=>'perusahaan'], function(){
+
+		Route::apiResource('/{perusahaan}/reviews','ReviewController');
+	
+});
